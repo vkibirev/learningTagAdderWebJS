7cb5485a9b0edeb9ac1ba41a83b5723c88064124
kibirev (Виктор Кибирев) <kibirev@office.local>
2018-07-19 15:56:48 +0300

    possibility to add atributes had been added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-let tag = function(tagName, text, children=[]) {
+let tag = function(tagName, text, children=[], attrs={}) {
     let item = document.createElement(tagName);
     if (text) {
         item.textContent = text;
@@ -6,16 +6,22 @@ let tag = function(tagName, text, children=[]) {
     for (let i = 0; i < children.length; ++i) {
         item.appendChild(children[i]);
     }
+
+    for (let attr in attrs) {
+        item.setAttribute(attr, attrs[attr])
+    }
     return item;
 }
 
+//in this code new tags should be written
+//order <tag>, text for tag (can be skipped), child tags (can be skipped), attributes (can be skipped)
 document.body.insertBefore(
     tag('div', '', [
-        tag('h1', 'H1 tag'),
-        tag('p', 'P tag'),
+        tag('h1', 'H1 tag', [], {style: 'color: red'}),
+        tag('p', 'P tag', [], {style: 'text-decoration: underline'}),
         tag('ul', '', [
             tag('li', 'first'),
-            tag('li', 'second'),
+            tag('li', 'second', [], {contenteditable: true}),
             tag('li', 'third'),
         ]),
 ]), document.querySelector('script'));
